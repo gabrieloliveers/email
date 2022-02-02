4467fd56c4df3eb3c486dbfe7542baf69ca60e3b
gabriel <gr.oliveira99@gmail.com>
2022-02-01 23:18:05 -0300

    01/02/2022

---
+++ main.py
@@ -0,0 +1,27 @@
+import smtplib
+import email
+import json
+
+
+
+message = email.message.Message()
+
+message['Subject'] = "Assunto"
+message['From'] = 'gr.oliveira99@gmail.com'
+message['To'] = 'alexsander.pereira@icloud.com' 
+message.add_header('Content-Type', 'text/html')
+
+with open("body.html", "r") as file_body:
+    body = file_body.read()
+
+message.set_payload(body)
+
+password = 'Bendez549178' 
+
+s = smtplib.SMTP('smtp.gmail.com: 587')
+
+s.starttls()
+s.login(message['From'], password)
+s.sendmail(message['From'], [message['To']], message.as_string().encode('utf-8'))
+    
+print('Email enviado')
