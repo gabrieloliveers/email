1cad69e300a2dfb4e351d94285ba062cf15726e8
gabriel <gr.oliveira99@gmail.com>
2022-02-15 10:43:37 -0300

    email recuperado

--- dados.py
+++ dados.py
@@ -1,14 +1,47 @@
 import email
 import json
-from msilib.schema import File
+from datetime import datetime
 
+from main import send_email
+
+# test = {}
+# test["nome"] = "Alex"
+# test["idate"] = 27
+# test2 = {"nome": "Alex", "idade": 27}
+# print(json.dumps(test)) # converte um dict em json
+
+data_today = datetime.today()
+data = data_today.strftime('%d/%m')
+            
 def check_data():
-
     with open('data.json') as json_file: 
-        data = json.load(json_file) 
+        for line in json_file:
+            user = json.loads(line)
+            date_birthday = datetime.strptime(user["data_nascimento"], '%d/%m/%Y').date()
+            niver = date_birthday.strftime('%d/%m')
+            if data == niver:
+                # print("é aniversário do {} que nasceu no dia {}".format(user["nome"], user["data_nascimento"]))
+                send_email(user["email"])
+                print(f'é aniversário do {user["nome"]} que nasceu no dia {user["data_nascimento"]}')
+            else:
+                print(f'não é aniversário do {user["nome"]} que nasceu no dia {user["data_nascimento"]}')
+              
 
-data = '{"nome": "Gabriel"; "data nascimento": "03/06/1999"; "email": "gr.oliveira99@gmail.com"}'
+def is_birthday():
+    with open('data.json') as json_file: 
+        for line in json_file:
+            user = json.loads(line)
+            date_birthday = datetime.strptime(user["data_nascimento"], '%d/%m/%Y').date()
+            niver = date_birthday.strftime('%d/%m')
+            if data == niver:
+               return user
+            else:
+               return None
 
-file = json.loads(data)
+check_data()
 
-print(file['email']) 
+user = is_birthday()
+if user:
+    send_email(user["email"])
+else:
+    pass
--- main.py
+++ main.py
@@ -2,26 +2,25 @@ import smtplib
 import email
 import json
 
+def send_email(emailto):       
+    message = email.message.Message()
+            
+    message['Subject'] = "Assunto"
+    message['From'] = 'gr.oliveira99@gmail.com'
+    message['To'] = emailto
+    message.add_header('Content-Type', 'text/html')
 
+    with open("body.html", "r") as file_body:
+            body = file_body.read()
 
-message = email.message.Message()
+    message.set_payload(body)
 
-message['Subject'] = "Assunto"
-message['From'] = 'gr.oliveira99@gmail.com'
-message['To'] = 'alexsander.pereira@icloud.com' 
-message.add_header('Content-Type', 'text/html')
+    password = 'Bendez5491781208' 
 
-with open("body.html", "r") as file_body:
-        body = file_body.read()
+    s = smtplib.SMTP('smtp.gmail.com: 587')
 
-message.set_payload(body)
-
-password = 'senha' 
-
-s = smtplib.SMTP('smtp.gmail.com: 587')
-
-s.starttls()
-s.login(message['From'], password)
-s.sendmail(message['From'], [message['To']], message.as_string().encode('utf-8'))
-    
-print('Email enviado')
+    s.starttls()
+    s.login(message['From'], password)
+    s.sendmail(message['From'], [message['To']], message.as_string().encode('utf-8'))
+        
+    print('Email enviado') 
