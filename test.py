40fe6f42fa75e7fda30506c7a632b444cc9c0ae9
gabrielolivers <gr.oliveira99@gmail.com>
2022-02-24 22:16:13 -0300

    TESTANDO (-) EM DATETIMES

--- test.py
+++ test.py
@@ -1,31 +1,32 @@
 #JEITO 1
-from datetime import date
+#from datetime import date
 
-data_niver = "10/05/1988" 
-current_date = date.today()
+#data_niver = "10/05/1988" 
+#current_date = date.today()
  
-print("A idade é : ",current_date - data_niver)
+#print("A idade é : ",current_date - data_niver)
 
 
 #======================= TENTANDO SUBTRAIR OS DATETIMES E APANHANDO LEGAL KKKKK ====================================================
 
 #JEITO 2
 import json
-from datetime import datetime, date
+from datetime import datetime
 
-current_date = date.today
+current_date = datetime.today()
 
-def idade(current_date, date_birthday):
+def idade(current_date):
     
     with open('data.json') as json_file: 
         for line in json_file:
             user = json.loads(line)
             date_birthday = datetime.strptime(user["data_nascimento"], '%d/%m/%Y').date()
             
-            niver = date_birthday.strftime('%d/%m/%Y')
+            # niver = date_birthday.strftime('%d/%m/%Y')
             
             date_today = datetime.strftime(current_date, "%Y")
-            date_niver = datetime.strftime(niver, "%Y")
+            date_niver = datetime.strftime(date_birthday, "%Y")
     
-            print("A idade é:",date_today - date_niver)
-    
+            print("A idade é:",int(date_today) - int(date_niver))
+
+idade(current_date)
